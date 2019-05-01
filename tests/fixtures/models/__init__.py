7abfb02cb9a9fce59a6cead3a82bfbc71a08f23e
tsan <ahtsans@gmail.com>
2019-05-01 10:07:35 -0700

    Coverage with tests

--- tests/fixtures/models/__init__.py
+++ tests/fixtures/models/__init__.py
@@ -1,10 +1,13 @@
 from tests.fixtures.models.simple_cnn_model import SimpleCNNModel
+from tests.fixtures.models.simple_cnn_model_with_max_pooling import (
+    SimpleCNNModelWithMaxPooling)
 from tests.fixtures.models.simple_gaussian_mlp_model import (
     SimpleGaussianMLPModel)
 from tests.fixtures.models.simple_mlp_model import SimpleMLPModel
 
 __all__ = [
     'SimpleCNNModel',
+    'SimpleCNNModelWithMaxPooling',
     'SimpleGaussianMLPModel',
     'SimpleMLPModel',
 ]
